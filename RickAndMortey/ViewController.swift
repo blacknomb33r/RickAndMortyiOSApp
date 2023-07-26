4c9153a4cd1a86ac0c2b35338dfab9a83ef27cae
Sebastian <94306815+blacknomb33r@users.noreply.github.com>
2023-07-26 17:15:14 +0200

    Initial Commit

---
+++ RickAndMortey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortey
+//
+//  Created by Sebastian Schulze on 7/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
