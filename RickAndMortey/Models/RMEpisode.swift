b3a30b73f0cb16472bbd45dc5607107741ee92a6
Sebastian <94306815+blacknomb33r@users.noreply.github.com>
2023-08-01 00:02:28 +0200

    Minor changesf

--- RickAndMortey/Models/RMEpisode.swift
+++ RickAndMortey/Models/RMEpisode.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
+/// Declaring the struct for episodes (answer from the api request)
 struct RMEpisode: Codable {
      let id: Int
      let name: String
      let air_date: String
-     let episode: [String]
+     let episode: [String] ///Array of strings
      let url: String
      let created: String
 }
